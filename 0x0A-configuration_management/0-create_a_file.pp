ff771cb7babf91fe95b65424fa003bf1153eabbe
Julius Kamara <juliuskamara79@gmail.com>
2023-04-15 15:24:13 -0700

    This manifest creates a new file with permission, owner, group and a content in it

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,14 +1,12 @@
 # This project shows us how to use puppet manifest to create a file
-require 'uri'
 
-$file_content = 'I love Puppet\n'
-$escaped_file_content = URI::DEFAULT_PARSER.escape($file_content)
+$file_content = 'I love Puppet'
 
 file { '/tmp/school':
   ensure  =>  'file',
   mode    =>  '0744',
   owner   =>  'www-data',
   group   =>  'www-data',
-  content =>  $escaped_file_content,
+  content =>  $file_content
   }
 
