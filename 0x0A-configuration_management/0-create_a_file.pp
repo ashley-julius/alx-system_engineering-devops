54538b01b807433b1a95f0bed003defcff359136
julius-ashley <juliuskamra79@gmail.com>
2023-04-15 14:51:38 -0700

    Made some changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,14 @@
 # This project shows us how to use puppet manifest to create a file
+require 'uri'
+
+$file_content = 'I love Puppet\n'
+$escaped_file_content = URI::DEFAULT_PARSER.escape($file_content)
+
 file { '/tmp/school':
   ensure  =>  'file',
   mode    =>  '0744',
-  owner   =>  'WWW-data',
-  group   =>  'WWW-data',
-  content =>  'I love Puppet\n'
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  $escaped_file_content,
   }
 
